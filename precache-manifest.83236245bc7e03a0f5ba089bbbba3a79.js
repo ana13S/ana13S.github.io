52a1ad7ef0f7c631c5336ace43efc49a619120d7
Ana Selvaraj <ana@selvaraj.org>
2020-03-26 05:31:09 -0700

    Updates

---
+++ precache-manifest.83236245bc7e03a0f5ba089bbbba3a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857447b8b6c31fcc244cf5a6aa2750ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38d507461233a61d5faf",
+    "url": "/static/css/main.6e972565.chunk.css"
+  },
+  {
+    "revision": "232e43ba17547cf93789",
+    "url": "/static/js/2.eb258079.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb258079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d507461233a61d5faf",
+    "url": "/static/js/main.ee626c0d.chunk.js"
+  },
+  {
+    "revision": "f8e4a263387d1ce3a60b",
+    "url": "/static/js/runtime-main.a6c66e20.js"
+  }
+]);
